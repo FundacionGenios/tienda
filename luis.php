1da71e304173158133d7d4a6225815fe99bff697
Luis Eduardo Sinisterra <luis@fundaciongenios.org>
2023-09-30 17:25:32 -0500

    Luis

---
+++ luis.php
@@ -0,0 +1,12 @@
+<?php 
+
+    $primerNumero = 0;
+    $segundoNumero = 90;
+
+    if ($primerNumero > $segundoNumero) {
+        echo "El primer numero es mayor que el segundo.";
+    }else{
+        echo "El segundo numero es mayor que el primero.";
+    }
+
+?>
