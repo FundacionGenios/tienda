22f76da77a0896cf2022dc9f210681305cf1ba6a
Luis Eduardo Sinisterra <luis@fundaciongenios.org>
2023-10-01 12:13:34 -0500

    Actualizacion

--- luis.php
+++ luis.php
@@ -1,9 +1,9 @@
 <?php 
 
-    $primerNumero = 0;
-    $segundoNumero = 90;
+    $n1 = 0;
+    $n2 = 90;
 
-    if ($primerNumero > $segundoNumero) {
+    if ($n1 > $n2) {
         echo "El primer numero es mayor que el segundo.";
     }else{
         echo "El segundo numero es mayor que el primero.";
